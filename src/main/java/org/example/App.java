0a9445b9ff40c49a993278bef3133e08a56c9695
Amrithesh A <88173388+AmritheshA@users.noreply.github.com>
2023-12-24 16:42:51 +0530

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,9 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        // Hello Dev So this is a simple package for learning purpose 
     }
 }
